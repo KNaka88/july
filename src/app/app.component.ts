2c4d2941c007c98cad33c3967ab587ba9f599e19
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-07-01 12:52:14 -0700

    add video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule, ElementRef } from '@angular/core';
 import { MdlUppgradeElementDirective } from './mdl-uppgrade-element.directive';
 
 @Component({
@@ -6,6 +6,26 @@ import { MdlUppgradeElementDirective } from './mdl-uppgrade-element.directive';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    private today: number;
+    private mainVideo: string = "../../assets/video/demo.mp4";
+
+    constructor(private elementRef: ElementRef) {}
+
+    ngOnInit() {
+        // Set currrent date
+        this.today = Date.now();
+
+        // Autoplay Video
+        this.startVideo();
+    }
+
+
+    startVideo() {
+        let video = this.elementRef.nativeElement.querySelector('video');
+        video.addEventListener( "canplay", () => {
+            video.play();
+        });
+    }
+
 }
